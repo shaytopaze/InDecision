2fb967ab72e85c8c3f7332e64bdd188dc48df620
Nick Lee <nickoless.lee@gmail.com>
2018-02-10 01:20:43 +0000

    Function now pulls data from psql instead of hardcoded values. Added functionality to insert data

--- db-helpers.js
+++ db-helpers.js
@@ -5,40 +5,61 @@ const knex        = require("knex")(knexConfig[ENV]);
 
 // function to set rank based on position (needs to be in order)
 
-
-
 knex.select('*')
   .from('options')
   .where('poll_id', 7)
   .then((result) => {
-    console.log(result)
-});
+    // console.log(result)
+  });
 
-// example array of 'options'
+// example array of 'options'. Rank table:  ID, OPTIONS_ID, RANK
 
-[ anonymous { id: 6, title: '1', description: '', poll_id: 7 },
-  anonymous { id: 7, title: '2', description: '', poll_id: 7 },
-  anonymous { id: 8, title: '3', description: '', poll_id: 7 },
-  anonymous { id: 9, title: '4', description: '', poll_id: 7 },
-  anonymous { id: 10, title: '5', description: '', poll_id: 7 } ]
+// const dummyData = [
+//   { id: 6, title: '1', description: '', poll_id: 7 },
+//   { id: 7, title: '2', description: '', poll_id: 7 },
+//   { id: 8, title: '3', description: '', poll_id: 7 },
+//   { id: 9, title: '4', description: '', poll_id: 7 },
+//   { id: 10, title: '5', description: '', poll_id: 7 } ]
+
+const rank = () => {
+    
+  knex('*')
+    .from('options')
+    .where('poll_id', 7)
+    .then((result) => {
+
+      result.forEach(function(element){
+        const option_id = element.id;
+        const position = result.indexOf(element);
+        const ranking = (result.length - position);
+        console.log('Display if rank function works: ', ranking);
+        
+        knex('rankings')
+          .insert({option_id: option_id, rank: ranking})
+          .then((result) => {
+
+          });
+      });
+    });
+};
 
 // example array for rank
 
-const options = ['a', 'b', 'c', 'd', 'e'];
+// const options = ['a', 'b', 'c', 'd', 'e'];
 
-const rank = () => {
-  options.forEach(function(element){
-    position = options.indexOf(element);
-    console.log(options.length - position)    ;
-  }); 
-};
+// const rank = () => {
+//   options.forEach(function(element){
+//     position = options.indexOf(element);
+//     console.log(options.length - position)    ;
+//   });
+// };
 
-rank()
+// rank();
 
 // test query
 
-// knex.select('*')
-//   .from('options')
-//   .then((result) => {
-//     console.log(result)
-// });
+knex.select('*')
+  .from('rankings')
+  .then((result) => {
+    console.log(result);
+  });
