7324ac50ecd05e86a459317a7b7ac8bc69e230a9
Pablo Aredu <pablo.adu83@gmail.com>
2018-02-10 18:23:51 +0000

    Added sortable buttons to vote page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,52 @@
 $(() => {
+
+
+
+  // $('#sortable').sortable({
+  //   update: function(event, ui) {
+  //     var option = $(this).sortable('toArray');
+  //     // $.get('update-sort.cfm', {fruitOrder:fruitOrder});
+  //   console.log(option);
+  //   }
+  });
+
+  $('button example id').on('click',function(e){
+    $('#sortable').toArray();
+  });
+
+
+
+  // var $messageEmptyTweet = $('<p>This is working!</p>');
+  //   $('.sticky-top').append($messageEmptyTweet);
+
+  // $( function sortOptions() {
+      // $("#sortable").sortable({
+      //   cancel: ".disable-sort"
+ });
+
+
+  // function giveArrayRanking( option ) {
+  //   var array = [];
+  //   for ( var i = 0; i < option.length; i++ ) {
+  //     array.push(option[i].innerHTML);
+  //   }
+  //   $( "span" ).text( array.join( " " ) );
+  //   console.log (array);
+  // }
+  // var option = $( "li" ).toArray();
+
+  // giveArrayRanking(option);
+  // var test = $("#sortable li").toArray().reverse();
+  // console.log(test);
+
+// });
+
+
+
+
+
+
+
   // The following is dummy code to demonstrate using ajax to access the users route, which we don't use.
   // $.ajax({
   //   method: "GET",
@@ -8,6 +56,6 @@ $(() => {
   //     $("<div>").text(user.name).appendTo($("body"));
   //   }
   // });;
-});
+
 
 //Create ranking system
