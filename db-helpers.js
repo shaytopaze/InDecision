c692db80ba9ca9fa55f4643a373e41e1c8296d30
Nick Lee <nickoless.lee@gmail.com>
2018-02-10 00:24:47 +0000

    Additional changes to rank function, imported dummy option data to manage queries with function

--- db-helpers.js
+++ db-helpers.js
@@ -5,19 +5,40 @@ const knex        = require("knex")(knexConfig[ENV]);
 
 // function to set rank based on position (needs to be in order)
 
-const dummy = [1, 2, 3, 4, 5, 6, 7];
 
-const rank = () => {
-  dummy.forEach(function(element){
-    position = dummy.indexOf(element);
-    console.log(dummy.length - position)    
-  }); 
-};
-
-// test query
 
 knex.select('*')
   .from('options')
+  .where('poll_id', 7)
   .then((result) => {
     console.log(result)
 });
+
+// example array of 'options'
+
+[ anonymous { id: 6, title: '1', description: '', poll_id: 7 },
+  anonymous { id: 7, title: '2', description: '', poll_id: 7 },
+  anonymous { id: 8, title: '3', description: '', poll_id: 7 },
+  anonymous { id: 9, title: '4', description: '', poll_id: 7 },
+  anonymous { id: 10, title: '5', description: '', poll_id: 7 } ]
+
+// example array for rank
+
+const options = ['a', 'b', 'c', 'd', 'e'];
+
+const rank = () => {
+  options.forEach(function(element){
+    position = options.indexOf(element);
+    console.log(options.length - position)    ;
+  }); 
+};
+
+rank()
+
+// test query
+
+// knex.select('*')
+//   .from('options')
+//   .then((result) => {
+//     console.log(result)
+// });
